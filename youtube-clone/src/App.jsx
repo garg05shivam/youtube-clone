7802b5a1dd3b36570c19d14cf6fd1ca976f7f417
SHIVAM GARG <148767875+garg05shivam@users.noreply.github.com>
2026-02-18 17:12:27 +0530

    app.jsx

---
+++ youtube-clone/src/App.jsx
@@ -0,0 +1,23 @@
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Watch from "./pages/Watch";
+import Upload from "./pages/Upload";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/watch/:id" element={<Watch />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/search/:query" element={<Search />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
